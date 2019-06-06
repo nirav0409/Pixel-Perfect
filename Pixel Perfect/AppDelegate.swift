7b9d5ff43f93aec775d0c5050e4923f7f034ea27
nirav0409 <patelnirav44@gmail.com>
2019-06-06 14:15:04 +0530

    Initial Commit

---
+++ Pixel Perfect/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Pixel Perfect
+//
+//  Created by Nirav Patel on 06/06/19.
+//  Copyright © 2019 Nirav Patel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
